4e2aaf0120f65db5474956907a598294ac02b8d7
iberg <isaac.berg@myemail.indwes.edu>
2015-01-20 21:30:53 -0500

    Made the censorifyib.js package
    Made censorifyib.js package according to the instructions I had been
    given. It should work given that 99% of it is exactly the same as
    another working javascript thing.

---
+++ censorifyib/README.md
@@ -0,0 +1,4 @@
+READ ME - PLEASE READ ME
+
+- I actually don't know what goes here -
+- I just sort of made the file because I was told I needed one -
---
+++ censorifyib/censortext.js
@@ -0,0 +1,25 @@
+var censoredWords = ["sad", "bad", "mad", "glad", "plaid", "dad", "pad", "rad", "lad"];
+var customCensoredWords = [];
+function censor(inStr) {
+   for (idx in censoredWords) 
+   {
+     inStr = inStr.replace(censoredWords[idx], "****");
+   }
+   for (idx in customCensoredWords) 
+   {
+     inStr = inStr.replace(customCensoredWords[idx], "****");
+   }
+  return inStr;
+}
+
+function addCensoredWord(word){
+   customCensoredWords.push(word);
+}
+
+function getCensoredWords(){
+   return censoredWords.concat(customCensoredWords);
+}
+
+exports.censor = censor;
+exports.addCensoredWord = addCensoredWord;
+exports.getCensoredWords = getCensoredWords; 
